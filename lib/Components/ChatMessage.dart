cc9477d624725e955a2945ce215e771f25c6cf4c
net00 <53622316+net00-1@users.noreply.github.com>
2024-10-25 22:22:21 -0600

    Force Safari to open

--- lib/Components/ChatMessage.dart
+++ lib/Components/ChatMessage.dart
@@ -103,7 +103,7 @@ class ChatMessage extends StatelessWidget {
                   ),
                   recognizer: TapGestureRecognizer()
                     ..onTap = () async {
-                      await launch(Uri.parse(token.data).toString());
+                      await launch(Uri.parse(token.data).toString(), false);
                     },
                 ),
                 TextSpan(text: ' '),
